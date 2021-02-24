2ee0a7b22d8c64ff104f9b22c82b1a5722d5b958
Ranny Vuong <rannyvuong80@gmail.com>
2021-02-24 16:02:08 -0500

    Add files via upload

---
+++ Scripts/404.js
@@ -0,0 +1,8 @@
+var loaded = "Website has been successfully loaded";
+var errorMsg = "This page cannot be found.";
+
+// Notice that the page has been loaded
+console.log(loaded);
+
+// Notice that the page cannot be found
+console.warn(errorMsg);
---
+++ Scripts/Index.js
@@ -0,0 +1,4 @@
+var loaded = "Website has been successfully loaded";
+
+// Notice that the page has been loaded
+console.log(loaded);
